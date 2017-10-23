a986fde7f9a946495b1a29351dec168da85f9560
Alex Chew <alex@alexchew.net>
2017-10-22 19:30:13 -0700

    Various small speed improvements

--- conformal.cpp
+++ conformal.cpp
@@ -16,9 +16,11 @@ typedef Point2f Pixel;
  * Returns a complex plane of the specified size, which has center at
  * approximately (0, 0) and is scaled down by the specified scale.
  */
-arma::cx_mat base_plane(int rows, int cols, double scale) {
-  const int mid_x = cols / 2;
-  const int mid_y = rows / 2;
+arma::cx_mat base_plane(const int rows, const int cols,
+    const double scale,
+    const arma::cx_float offset) {
+  const int mid_x = offset.real();
+  const int mid_y = offset.imag();
 
   arma::mat A = arma::repmat(
       arma::regspace<arma::rowvec>(-mid_x, -mid_x + cols - 1), rows, 1);
@@ -27,27 +29,27 @@ arma::cx_mat base_plane(int rows, int cols, double scale) {
   return arma::cx_mat(A, B) / scale;
 }
 
-inline arma::fmat matmod(arma::fmat a, int k) {
-  return a - k * arma::floor(a / k);
+inline arma::fmat posmod(const arma::fmat& a, const int k) {
+  arma::fmat adk = a / k;
+  arma::fmat floor_adk_dec = arma::floor(adk) - 1;
+  return (a - k * floor_adk_dec) - k * arma::floor(adk - floor_adk_dec);
 }
 
-inline arma::fmat posmod(arma::fmat a, int k) {
-  return matmod(matmod(a, k) + k, k);
-}
-
-void image_plane(const arma::cx_fmat& base, const double scale,
+void image_plane(const arma::cx_fmat& base,
+    const double scale,
+    const arma::cx_float offset,
     Mat& out_x, Mat& out_y) {
-  const arma::cx_float offset =
-    arma::cx_float(base.n_cols / 2, base.n_rows / 2);
-  arma::cx_fmat img = base * scale + offset;
-  std::cout << "  posmod" << std::endl;
-  arma::fmat re = posmod(arma::real(img), base.n_cols).t();
-  arma::fmat im = posmod(arma::imag(img), base.n_rows).t();
+  std::cout << "  norm" << std::endl;
+  arma::fmat norm_re = arma::real(base) * scale + offset.real();
+  arma::fmat norm_im = arma::imag(base) * scale + offset.imag();
+  std::cout << "  mod" << std::endl;
+  arma::fmat mod_re = posmod(norm_re, base.n_cols).t();
+  arma::fmat mod_im = posmod(norm_im, base.n_rows).t();
 
-  std::cout << "  copy to Mat" << std::endl;
+  std::cout << "  copy" << std::endl;
   Size sz(base.n_cols, base.n_rows);
-  out_x = Mat(sz, CV_32FC1, const_cast<float *>(re.memptr())).clone();
-  out_y = Mat(sz, CV_32FC1, const_cast<float *>(im.memptr())).clone();
+  out_x = Mat(sz, CV_32FC1, const_cast<float *>(mod_re.memptr())).clone();
+  out_y = Mat(sz, CV_32FC1, const_cast<float *>(mod_im.memptr())).clone();
 }
 
 int main(int argc, char **argv) {
@@ -63,9 +65,11 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  double scale = min(src.cols, src.rows) / 2.0;
+  const double scale = min(src.cols, src.rows) / 2.0;
+  const arma::cx_float offset = arma::cx_float(src.cols / 2, src.rows / 2);
+
   arma::cx_fmat base = arma::conv_to<arma::cx_fmat>::from(
-      base_plane(src.rows, src.cols, scale));
+      base_plane(src.rows, src.cols, scale, offset));
   Mat map_x, map_y;
 
   Mat dst;
@@ -73,9 +77,9 @@ int main(int argc, char **argv) {
 
   VideoWriter vw("out.mp4", CV_FOURCC('H', '2', '6', '4'), 30, src.size());
 
-  for (float i = 0.1; i < 2.0; i += 0.04) {
+  for (float i = 0.1; i < 2.0; i += 0.08) {
     std::cout << "Computing map for z^" << i << std::endl;
-    image_plane(arma::pow(base, i), scale, map_x, map_y);
+    image_plane(arma::pow(base, i), scale, offset, map_x, map_y);
     std::cout << "Remapping image for z^" << i << std::endl;
     remap(src, dst, map_x, map_y,
         INTER_LINEAR, BORDER_CONSTANT, Scalar(0, 0, 0));
